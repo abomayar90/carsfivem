3c32cf7f74db770583c0f37bdbcf9e4bea74e93c
abomayar90 <94208216+abomayar90@users.noreply.github.com>
2026-07-24 12:21:29 +0300

    VehCatalog: garage (10 vehicles)

--- garage.lua
+++ garage.lua
@@ -1,6 +1,13 @@
 -- VehCatalog auto-generated
 return {
     ["tra1"] = {"tra1",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra1_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra10"] = {"tra10",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra10_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra11"] = {"tra11",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra11_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
     ["tra3"] = {"tra3",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra3_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
     ["tra4"] = {"tra4",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra4_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra5"] = {"tra5",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra5_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra6"] = {"tra6",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra6_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra7"] = {"tra7",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra7_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra8"] = {"tra8",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra8_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra9"] = {"tra9",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra9_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
 }
