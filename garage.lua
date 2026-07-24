c4a6c29567ce717e248bdf7dad42f8cc89eef1b7
abomayar90 <94208216+abomayar90@users.noreply.github.com>
2026-07-24 03:59:34 +0300

    VehCatalog: garage (3 vehicles)

---
+++ garage.lua
@@ -0,0 +1,6 @@
+-- VehCatalog auto-generated
+return {
+    ["tra1"] = {"tra1",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra1_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra3"] = {"tra3",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra3_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["tra4"] = {"tra4",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/tra4_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+}
