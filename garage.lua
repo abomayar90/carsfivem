9876266fd73b29e7c64bee52b5c1e7fbe9a65243
abomayar90 <94208216+abomayar90@users.noreply.github.com>
2026-08-08 00:20:02 +0300

    VehCatalog: garage session (11 vehicles)

--- garage.lua
+++ garage.lua
@@ -1,8 +1,14 @@
 -- VehCatalog auto-generated (last session only)
 return {
-    ["1ambulance"] = {"1ambulance",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/1ambulance_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["2ambulance"] = {"2ambulance",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/2ambulance_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["3ambulance"] = {"3ambulance",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/3ambulance_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["4ambulance"] = {"4ambulance",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/4ambulance_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["5ambulance"] = {"5ambulance",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/5ambulance_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb900"] = {"amb900",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb900_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb901"] = {"amb901",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb901_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb902"] = {"amb902",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb902_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb903"] = {"amb903",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb903_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb904"] = {"amb904",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb904_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb905"] = {"amb905",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb905_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb906"] = {"amb906",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb906_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb907"] = {"amb907",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb907_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb908"] = {"amb908",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb908_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb909"] = {"amb909",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb909_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["amb910"] = {"amb910",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/amb910_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
 }
