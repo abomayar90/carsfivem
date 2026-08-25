46eed0ddd1c9d046fcbabbfad11d509833d44003
abomayar90 <94208216+abomayar90@users.noreply.github.com>
2026-08-25 23:33:11 +0300

    VehCatalog: garage session (12 vehicles)

--- garage.lua
+++ garage.lua
@@ -1,10 +1,15 @@
 -- VehCatalog auto-generated (last session only)
 return {
-    ["abrams"] = {"abrams",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/abrams_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["abramsb"] = {"abramsb",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/abramsb_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["akula"] = {"akula",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/akula_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["autarch"] = {"autarch",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/autarch_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["avenger"] = {"avenger",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/avenger_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["avenger2"] = {"avenger2",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/avenger2_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
-    ["blak200"] = {"blak200",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/blak200_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["CafeHelloKitty"] = {"CafeHelloKitty",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/CafeHelloKitty_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["Challengergirls"] = {"Challengergirls",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/Challengergirls_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["GODzHAYABUSA"] = {"GODzHAYABUSA",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/GODzHAYABUSA_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["ag_silverboard"] = {"ag_silverboard",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/ag_silverboard_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["avtr"] = {"avtr",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/avtr_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["bbdawn"] = {"bbdawn",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/bbdawn_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["bmwhommage"] = {"bmwhommage",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/bmwhommage_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["cafe_weed"] = {"cafe_weed",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/cafe_weed_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["cat_car"] = {"cat_car",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/cat_car_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["g650barbie"] = {"g650barbie",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/g650barbie_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["lambobarbie"] = {"lambobarbie",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/lambobarbie_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
+    ["porschebarbie"] = {"porschebarbie",0,"<img src='https://raw.githubusercontent.com/abomayar90/carsfivem/main/porschebarbie_custom_setup.png' /><br/><br/>Top Speed: 0 KM/h<br/>"},
 }
